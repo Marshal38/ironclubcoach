732628f7a2c71598f763765e35377c673aff4101
Marshal38 <vladislav.zhukov52@gmail.com>
2026-07-23 23:16:58 +0500

    изменил scale на сингл карте

--- src/coachs/CoachSingle.jsx
+++ src/coachs/CoachSingle.jsx
@@ -8,12 +8,12 @@ function CoachSingle({ coach }) {
     !image || imgError ? '/blank-profile-picture-973460_1280-1.jpg' : image;
 
   return (
-    <div className="flex flex-col items-center mt-5 rounded-xl hover:shadow-2xl/100 hover:shadow-emerald-900">
+    <div className="flex flex-col items-center mt-5 rounded-xl hover:scale-120 ">
       <img
         src={imageSrc}
         alt={name}
         onError={() => setImgError(true)}
-        className="w-full max-w-48 sm:max-w-70 aspect-square object-cover rounded-2xl shadow-lg/50"
+        className="w-full max-w-48 sm:max-w-70 aspect-square object-cover rounded-2xl shadow-lg/50 "
       />
       <span className="text-center font-bold text-lg sm:text-3xl mt-2 sm:mt-4 mb-2 sm:mb-5">
         {name}
