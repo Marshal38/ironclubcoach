c1228cfacd7a8e58c16873c0f556447fec302a4a
Marshal38 <vladislav.zhukov52@gmail.com>
2026-08-06 01:03:00 +0500

    Подкорректировал кнопки тренеры и расписание

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import Logo from './Logo';
 
 function Header() {
@@ -6,18 +6,30 @@ function Header() {
     <div className="flex flex-col lg:flex-row items-center sm:justify-between gap-4 px-[5%] mt-[3%]">
       <Logo />
       <div className="flex flex-wrap items-center justify-center gap-3 sm:gap-8 mt-4 text-base sm:text-2xl">
-        <Link
+        <NavLink
           to="/?affiliate=all"
-          className="px-4 py-2 sm:px-6 sm:py-4 border-2 sm:border-4 rounded-full bg-[#0b3306] text-white tracking-wide transition hover:bg-[#86cf7e] hover:text-black font-semibold text-center"
+          className={({ isActive }) =>
+            `px-4 py-2 sm:px-6 sm:py-4 border-2 sm:border-4 rounded-full transition font-semibold text-center ${
+              isActive
+                ? 'bg-[#86cf7e] text-black'
+                : 'bg-[#0b3306] text-white hover:bg-[#86cf7e] hover:text-black'
+            }`
+          }
         >
           Тренеры
-        </Link>
-        <Link
+        </NavLink>
+        <NavLink
           to="/shedule"
-          className="px-4 py-2 sm:px-6 sm:py-4 border-2 sm:border-4 rounded-full bg-[#0b3306] text-white transition hover:bg-[#86cf7e] hover:text-black font-semibold text-center"
+          className={({ isActive }) =>
+            `px-4 py-2 sm:px-6 sm:py-4 border-2 sm:border-4 rounded-full transition font-semibold text-center ${
+              isActive
+                ? 'bg-[#86cf7e] text-black'
+                : 'bg-[#0b3306] text-white hover:bg-[#86cf7e] hover:text-black'
+            }`
+          }
         >
           Расписание
-        </Link>
+        </NavLink>
       </div>
     </div>
   );
