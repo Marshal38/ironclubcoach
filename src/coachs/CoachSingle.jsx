3cede10709225a34393e3516c6a890fef852e50d
Marshal38 <vladislav.zhukov52@gmail.com>
2026-08-05 23:10:45 +0500

    изменил размер фото

--- src/coachs/CoachSingle.jsx
+++ src/coachs/CoachSingle.jsx
@@ -13,7 +13,7 @@ function CoachSingle({ coach }) {
         src={imageSrc}
         alt={name}
         onError={() => setImgError(true)}
-        className="w-full max-w-48 sm:max-w-70 aspect-square object-cover rounded-2xl shadow-lg/50 "
+        className="w-full max-w-48 sm:max-w-65 aspect-square object-cover object-[center_32%] rounded-2xl shadow-lg/50 "
       />
       <span className="relative text-center font-bold text-lg sm:text-3xl mt-3 sm:mt-4 mb-2 sm:mb-5 after:content-[''] after:absolute after:left-1/2 after:bottom-0 after:h-0.5 after:w-full after:-translate-x-1/2 after:origin-center after:scale-x-0 after:bg-[#0b3306] after:transition-transform after:duration-300 group-hover:after:scale-x-100">
         {name}
